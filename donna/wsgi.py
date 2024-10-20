16dcf6f12bd804725cec02543737e7b1a4a2abeb
Swapnil Ranjan <swranjan@Swapnils-MacBook-Pro.local>
2024-10-21 00:07:14 +0530

    print

--- donna/wsgi.py
+++ donna/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'donna.deployment' #if 'WEBSITE_HOSTNAME' in os.environ else 'donna.settings'
-
+settings_module = 'donna.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'donna.settings'
+print(settings_module)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
