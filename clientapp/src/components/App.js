fed72b6aebe689c9351a011839cf51979a7bdabb
Swapnil Ranjan <swranjan@Swapnils-MacBook-Pro.local>
2024-10-21 01:24:46 +0530

    add clientApp build

--- clientapp/src/components/App.js
+++ clientapp/src/components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
         <div>
-      <h1>Donna React 2</h1>
+      <h1>Donna React</h1>
       <LongInputComponent/>
       </div>
     );
